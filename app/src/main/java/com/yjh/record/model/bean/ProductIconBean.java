43e2e5a0c910f09450af0095fe1294e032461dfd
youjiahui <yjh18970369486@163.com>
2026-07-22 19:10:55 +0800

    1.添加商品界面支持设置商品 Icon。

---
+++ app/src/main/java/com/yjh/record/model/bean/ProductIconBean.java
@@ -0,0 +1,27 @@
+package com.yjh.record.model.bean;
+
+public class ProductIconBean {
+    private String title;
+    private int iconRes;
+
+    public ProductIconBean(String title, int iconRes) {
+        this.title = title;
+        this.iconRes = iconRes;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getIconRes() {
+        return iconRes;
+    }
+
+    public void setIconRes(int iconRes) {
+        this.iconRes = iconRes;
+    }
+}
--- app/src/main/java/com/yjh/record/ui/activity/AddProductActivity.java
+++ app/src/main/java/com/yjh/record/ui/activity/AddProductActivity.java
@@ -5,9 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import com.yjh.base.uikit.widget.dialog.bottom.GridPanelBottomDialog;
 import com.yjh.record.R;
 import com.yjh.record.contract.AddProductContract;
 import com.yjh.record.databinding.AcAddProductBinding;
+import com.yjh.record.model.bean.ProductIconBean;
 import com.yjh.record.presenter.AddProductPresenter;
 import com.yjh.base.core.annotation.InjectPresenter;
 import com.yjh.base.core.annotation.IntentParam;
@@ -16,6 +19,9 @@ import com.yjh.base.uikit.widget.spinner.DateSpinner;
 import com.yjh.base.uikit.widget.titleBar.TitleBar;
 import com.yjh.base.utils.util.ConvertUtils;
 import com.yjh.base.utils.util.ToastUtils;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class AddProductActivity extends BaseActivity<AcAddProductBinding> implements AddProductContract.View {
 
@@ -26,7 +32,11 @@ public class AddProductActivity extends BaseActivity<AcAddProductBinding> implem
     private EditText etProductPrice;
     private EditText etPurchaseDate;
     private Button btnSubmitProduct;
+    private ImageView ivProductIcon;
     private String dateStr;
+
+    // 记录当前选中的图标 ID，0 表示未选择/无图标
+    private int selectedIconRes = 0;
 
     @InjectPresenter
     AddProductPresenter addProductPresenter;
@@ -44,6 +54,7 @@ public class AddProductActivity extends BaseActivity<AcAddProductBinding> implem
         btnSubmitProduct=findViewById(R.id.btn_submit_product);
         etProductName=findViewById(R.id.et_product_name);
         etProductPrice=findViewById(R.id.et_product_price);
+        ivProductIcon=binding.ivProductIcon;
     }
 
     @SuppressLint("SetTextI18n")
@@ -86,6 +97,41 @@ public class AddProductActivity extends BaseActivity<AcAddProductBinding> implem
             addProductPresenter.saveProduct(productName, price,dateStr,1);
 
         },btnSubmitProduct);
+
+        setClick(v->{
+            ProductIconBean item01=new ProductIconBean("键盘",R.drawable.pic_keyboard);
+            ProductIconBean item02=new ProductIconBean("相机",R.drawable.pic_camera);
+            ProductIconBean item03=new ProductIconBean("手柄",R.drawable.pic_controller);
+            ProductIconBean item04=new ProductIconBean("U盘",R.drawable.pic_usb);
+            ProductIconBean item05=new ProductIconBean("鼠标",R.drawable.pic_mouse);
+            ProductIconBean item06=new ProductIconBean("鼠标1",R.drawable.pic_mouse);
+            ProductIconBean item07=new ProductIconBean("鼠标2",R.drawable.pic_mouse);
+            ProductIconBean item08=new ProductIconBean("鼠标3",R.drawable.pic_mouse);
+            ProductIconBean item09=new ProductIconBean("鼠标4",R.drawable.pic_mouse);
+            ProductIconBean item10=new ProductIconBean("鼠标5",R.drawable.pic_mouse);
+            ProductIconBean item11=new ProductIconBean("鼠标6",R.drawable.pic_mouse);
+            ProductIconBean item12=new ProductIconBean("鼠标7",R.drawable.pic_mouse);
+            ProductIconBean item13=new ProductIconBean("鼠标8",R.drawable.pic_mouse);
+            ProductIconBean item14=new ProductIconBean("鼠标9",R.drawable.pic_mouse);
+            ProductIconBean item15=new ProductIconBean("鼠标10",R.drawable.pic_mouse);
+            List<ProductIconBean> menus = Arrays.asList(item01,item02,item03,item04,item05,item06,item07,item08,item09,item10,
+                    item11,item12,item13,item14,item15);
+
+            GridPanelBottomDialog.newInstance(
+                    3, 4, menus,
+                    (binding, data, position) -> {
+                        binding.tvItemName.setText(data.getTitle());
+                        binding.ivItemIcon.setImageResource(data.getIconRes());
+                    },
+                    (data, globalPosition) -> {
+                        ivProductIcon.setImageResource(data.getIconRes());
+                        //清除 pic_set_product_icon
+                        binding.ivProductIcon.setBackground(null);
+                    }
+
+            ).showTitle(true).show(getSupportFragmentManager(), "dialog_select_product_icon");
+
+        },ivProductIcon);
     }
 
     @Override
